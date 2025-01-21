19f0919f3dd30376ece9f17d87186b26dfe29724
divya-1604 <manchandadivya04@gmail.com>
2025-01-21 16:09:07 +0530

    Building a Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 // import "./App.css";
 
 function App() {
@@ -10,8 +11,8 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav" bg="coral">
-          Nav
+        <GridItem area="nav">
+          <NavBar />
         </GridItem>
         <Show above="lg">
           <GridItem area="aside" bg="yellow">
